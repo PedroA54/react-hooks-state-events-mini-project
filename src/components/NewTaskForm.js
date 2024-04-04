9901caaf59e40624ffadac18234d81abe4a87bf5
Pedro Alarcon <pedroa54@icloud.com>
2024-04-03 23:44:00 -0500

    adds complete lab

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ onTaskFormSubmit, categories }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState(categories[0]);
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = { category, text };
+    onTaskFormSubmit(newTask);
+    setText("");
+  };
+
   return (
-    <form className="new-task-form">
+    <form onSubmit={handleSubmit} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" value={text} onChange={handleTextChange} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select value={category} onChange={handleCategoryChange}>
+          {categories.map((cat) => (
+            cat !== "All" && (
+              <option key={cat} value={cat}>
+                {cat}
+              </option>
+            )
+          ))}
         </select>
       </label>
-      <input type="submit" value="Add task" />
+      <button type="submit">Add task</button>
     </form>
   );
 }
